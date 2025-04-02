bef234991f6bd562cca44c9093f4fb8236cabf9c
FernandoROL <felhopez@gmail.com>
2025-04-02 17:44:53 -0300

    new attribute for products

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -7,6 +7,7 @@ export interface Product {
   code: string;
   price: number;
   model: string;
+  productUrl: string;
 }
 
 export class ProductRepository {
@@ -72,12 +73,13 @@ export class ProductRepository {
         },
         ConditionExpression: "attribute_exists(id)",
         ReturnValues: "UPDATED_NEW",
-        UpdateExpression: "set productName = :n, code = :c, price = :p, model = :m",
+        UpdateExpression: "set productName = :n, code = :c, price = :p, model = :m, productUrl = :u",
         ExpressionAttributeValues: {
             ":n": productNew.productName,
             ":c": productNew.code,
             ":p": productNew.price,
-            ":m": productNew.model
+            ":m": productNew.model,
+            ":u": productNew.productUrl
         }
     }).promise()
     
